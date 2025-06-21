78cef20d3d7ca3dc0cae0a0bb92d672f46eb847c
srarkar <rickdsarkar@gmail.com>
2025-06-21 18:05:46 -0400

    basic SHA256 (not file-based)

--- cleaner/hasher.py
+++ cleaner/hasher.py
@@ -1,1 +1,12 @@
 # simply computes hashes of files (NOT compare them)
+# consider various hashing protocols (MD5, SHA256 -- slower but very secure? even SHA348 or 512)
+# security is not really a concern --  focus on a protocol that works for large files
+import hashlib
+
+# returns hash of file located at provided path
+def compute_hash(path_to_file):
+    hash_obj = hashlib.sha256()
+    input_data = "This is the data to be hashed."
+    hash_obj.update(input_data.encode('utf-8'))
+    hash_obj = hash_obj.hexdigest()
+    return hash_obj
