656ae0e94ff2aaad79b23eca602ebee7bad75d80
srarkar <rickdsarkar@gmail.com>
2025-06-22 09:31:56 -0400

    exception handling for compute_hash()

--- cleaner/hasher.py
+++ cleaner/hasher.py
@@ -4,19 +4,37 @@ import hashlib
 
 # returns hash of file located at provided path
 # hash protocol is SHA256 for security
-def compute_hash(path_to_file):
+# returns None if hash fails to be computed -- mainly with opening the file
+def compute_hash(path):
+    if path is None:
+        return 
     # arbitrary buffer size
     BUF_SIZE = 65536  # 64 kb chunks
 
     hash_obj = hashlib.sha256()
+    try:
+        with open(path, 'rb') as file:
+            while True:
+                data = file.read(BUF_SIZE)
+                if not data:
+                    break
+                hash_obj.update(data)
+        return hash_obj.hexdigest()
+    except FileNotFoundError:
+        print(f"Error -- File not found: {path}")
+        return None
+    except IsADirectoryError:
+        print(f"Error -- Expected file, not directory: {path}")
+        return None
+    except PermissionError:
+        print(f"Error -- You do not have permission to access this file: {path}")
+        return None
+    except Exception as e:
+        print(f"Unknown error when accessing {path}: {e}")
+        return None
     
-    with open(path_to_file, 'rb') as file:
-        while True:
-            data = file.read(BUF_SIZE)
-            if not data:
-                break
-            hash_obj.update(data)
-    return hash_obj.hexdigest()
+def validate_hash(path, expected_hash):
+    return compute_hash(path) == expected_hash
 
 def compare_hashes(file1, file2):
     file1_hash = compute_hash(file1)
