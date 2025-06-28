0b10e0a958ecc367f47542d4f22395db61592233
srarkar <rickdsarkar@gmail.com>
2025-06-28 14:48:24 -0400

    program flow for reporter.py methods

--- cleaner/reporter.py
+++ cleaner/reporter.py
@@ -1,10 +1,15 @@
 # this will handle displaying the summary of changes from running the cli tool
 
-def print_report(storage: int, files: list):
-    # maybe files can be a dictionary, where it is mapped to the storage?
-    # print files that were deleted
-    print("Deleted files:")
-    for file in files:
-        print(" " + file)
-    print("Disk space cleared:")
-    print(" " + str(storage) + " bytes")
+def print_report(files: list):
+    # given a list of files, call on calculate report to get total storage deleted
+    # things to consider printing:
+    # total storage cleared
+    # number of duplicate groups found -- this is the number of keys in files_by_hash!
+    # number of files deleted
+    # number of symlinks made
+    pass
+    
+
+def calculate_report(files: lst):
+    # returns a dictionary with keys being fields like storage_saved, num_files_deleted, etc. called on by print_report
+    pass
